afeee5d315bdcf86e886bfb65100133c9ef86d53
SparshJain2000 <jainsparsh0801@gmail.com>
2021-05-16 00:14:37 +0530

    upload file on server

--- routes/files.js
+++ routes/files.js
@@ -1,89 +1,55 @@
 const express = require("express");
-const request = require("request");
+const router = express.Router();
 const fs = require("fs");
-const app = express();
+const middleware = require("../middleware/index");
 const multer = require("multer");
-const mongoose = require("mongoose");
-const crypto = require("crypto");
-const methodOverride = require("method-override");
-const GridFsStorage = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const path = require("path");
-const bodyParser = require("body-parser");
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
-const mongoUrl = process.env.DATABASE_URL;
-const conn = mongoose.createConnection(mongoUrl);
-let gfs;
-conn.once("open", function() {
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection("uploads");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./data/files");
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        cb(
+            null,
+            Date.now() +
+                "_" +
+                file.originalname.split(" ")[0] +
+                "." +
+                file.originalname.split(".")[1],
+        );
+    },
 });
-
-//storage
-const storage = new GridFsStorage({
-    url: mongoUrl,
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            const fileInfo = {
-                filename: file.originalname,
-                bucketName: "uploads"
-            };
-            resolve(fileInfo);
-        });
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(null, true);
     }
+};
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter,
 });
-const upload = multer({ storage });
 
 //download
 
-app.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.render("page");
 });
-app.post("/upload", upload.single("file"), function(req, res) {
-    console.log("uploaded");
-    res.redirect("/");
-});
-app.get("/files", function(req, res) {
-    gfs.files.find({}).toArray((err, file) => {
-        if (!file || file.length === 0) {
-            return res.status(404).json({ err: "No Files Exist" });
-        } else {
-            return res.json(file);
-        }
-    });
-});
+router.post(
+    "/upload",
+    middleware.isFaculty,
+    upload.single("file"),
+    function (req, res) {
+        console.log(req.body);
+        console.log(req.file);
+        console.log("uploaded");
+        res.redirect("/");
+    },
+);
 
-app.get("/show", (req, res) => {
-    gfs.collection("uploads");
-    var key = req.query.key;
-    gfs.files.find({ filename: key }).toArray(function(err, files) {
-        if (!files || files.length === 0) {
-            return res.status(404).json({
-                responseCode: 1,
-                responseMessage: "error"
-            });
-        }
-        var readstream = gfs.createReadStream({
-            filename: files[0].filename,
-            root: "uploads"
-        });
-        var name = files[0].filename;
-        var writestream = fs.createWriteStream(name);
-        readstream.pipe(writestream);
-
-        res.download(__dirname + "/" + name, name, function(err) {
-            if (err) throw err;
-            else console.log("check console");
-        });
-
-        res.set("Content-Type", files[0].contentType);
-    });
-});
-
-const port = 8080;
-app.listen(port, function() {
-    console.log("running");
-});
+module.exports = router;
